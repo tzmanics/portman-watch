07c3cc9028ba2833504f11dc01493607b6ecb746
tzmanics <tzmanics@gmail.com>
2017-02-09 00:24:04 -0500

    change model, styling & content

--- src/app/update-item.ts
+++ src/app/update-item.ts
@@ -1,10 +1,13 @@
 export class UpdateItem {
-  date: string;
-  //TODO: handle date
-  content: string;
-  author: string;
-  link: string;
-  tags: string;
+  author: string;// who published
+  callToAction: string;// info on how people can protest actions
+  context: string;// who or what is affected
+  date: string;// TODO: handle date
+  facebookContent: string;// content for facebook post link
+  headline: string;// unbiased description of action || vote
+  link: string;// url to source
+  twitterContent: string;// content for tweet link
+  tags: string;// list of relevant keywords {e.g. human rights, congress, etc.)
 }
 
 
--- src/app/update-items.ts
+++ src/app/update-items.ts
@@ -2,24 +2,36 @@ import { UpdateItem } from './update-item';
 
 export const UPDATE_ITEMS: any[] = [
   {
-    date: '1 FEB 2017',
-    content: 'Portman Votes to Confirm HHS (Rep. Tom Price R-Georgia) & Treasury Secretary Nominees (Steven Mnuchin)',
-    author:'tzmanics',
-    link:'http://www.portman.senate.gov/public/index.cfm/press-releases?ID=911926C4-4E80-42D1-A509-DD94CA4C3F9A',
-    tags:'cabinet members, secretary of health and human services, treasury secretary'
+    author: 'mjs',
+    callToAction: 'Call Rob and let him know what you think of this vote',
+    context: 'Rob Portman voted on Tuesday to silence Elizabeth Warren, senator from Massachusetts, for reading a 1986 letter from Corretta Scott King to the Senate Judiciary Committee regarding the nomination of Jeff Sessions for federal Judge. Warren was reading the letter on the Senate floor during debate over the confirmation of Senator Sessions for U.S. Attorney General. Rob agreed with the chair that Warren was in violation of rule XIX of the senate, which states that "No Senator in debate shall, directly or indirectly, by any form of words impute to another Senator or to other Senators any conduct or motive unworthy or unbecoming a Senator."',
+    date: '2/7/2017',
+    facebookContent: 'Portman silences reading of Coretta Scott King letter on senate floor',
+    headline: 'Portman silences reading of Coretta Scott King letter on senate floor',
+    link: 'https://www.senate.gov/legislative/LIS/roll_call_lists/roll_call_vote_cfm.cfm?congress=115&session=1&vote=00057',
+    twitterContent: '@senrobportman silences reading of Coretta Scott King letter on senate floor https://www.senate.gov/legislative/LIS/roll_call_lists/roll_call_vote_cfm.cfm?congress=115&session=1&vote=00057',
+    tags: 'cabinet, justice',
   },
   {
-    date: '23 JAN 2017',
-    content: 'Portman Announces Support for Rex Tillerson as Secretary of State',
-    author:'tzmanics',
-    link:'http://www.portman.senate.gov/public/index.cfm/2017/1/portman-announces-support-for-rex-tillerson-as-secretary-of-state',
-    tags:'secretary of state, cabinet members'
-  },
-  {
-    date: '23 JAN 2017',
-    content: 'Portman Joins Group of 29 Senators in Introducing Bill to Permanently Prohibit Federal Funding of Abortion',
-    author:'tzmanics',
-    link:'http://www.portman.senate.gov/public/index.cfm?p=press-releases&id=3C1B1799-7512-4D8B-A3BC-8F8A49F9B23F',
-    tags:'abortion, womens rights, federal funding'
+    author: 'mjs',
+    callToAction: 'Call Rob and let him know what you think of this vote',
+    context: 'Rob Portman voted on Tuesday to confirm Trump nominee Betsy DeVos as the Secretary of Education. DeVos is the chair of the pro-school-choice advocacy group American Federation for Children. School choice advocates work to pass legislation allowing the use of public funds for private education through the use of vouchers or similar mechanisms. This Washington Post piece has more on her record: https://www.washingtonpost.com/news/answer-sheet/wp/2016/12/08/a-sobering-look-at-what-betsy-devos-did-to-education-in-michigan-and-what-she-might-do-as-secretary-of-education/?utm_term=.ac9211628568',
+    date: '2/7/2017',
+    facebookContent: 'Portman votes to confirm Betsy DeVos for Secretary of Education',
+    headline: 'Portman votes to confirm Betsy DeVos for Secretary of Education',
+    link: 'https://www.senate.gov/legislative/LIS/roll_call_lists/roll_call_vote_cfm.cfm?congress=115&session=1&vote=00054',
+    twitterContent: '.@senrobportman votes to confirm Betsy DeVos for Secretary of Education when this is her record: https://www.washingtonpost.com/news/answer-sheet/wp/2016/12/08/a-sobering-look-at-what-betsy-devos-did-to-education-in-michigan-and-what-she-might-do-as-secretary-of-education/?utm_term=.ac9211628568 #noToDeVos',
+    tags: 'cabinet, education, donors'
   }
+//  {
+//    author:
+//    callToAction:
+//    context:
+//    date:
+//    facebookContent:
+//    headline:
+//    link:
+//    twitterContent:
+//    tags:
+//  }
 ];
